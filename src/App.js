5284a651e3359c3842df5cb6ef170c5effaa25bc
Gloria <kapxhiugloria44@gmail.com>
2024-05-26 10:09:03 +0200

    backend changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Signup from './components/Signup/Signup';
+import Navbar from './components/Navbar/Navbar';
+import React from 'react';
+
 
 function App() {
   return (
-    <div>
-     
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'element={<Login/>}></Route>
+      <Route path='/signup'element={<Signup/>}></Route>
+      <Route path='/navbar'element={<Navbar/>}></Route>
+    </Routes>
+    
+    </BrowserRouter>
+  )
 }
 
 export default App;
